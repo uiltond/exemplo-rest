1e4010c8a2c66f6bbdbe0c2bfbd2f18b2e96c1bf
Usuario1234 <uilton.duarte@gmail.com>
2024-02-22 16:01:13 -0300

    inclusão do projeto

---
+++ exemplo.restassured.reqres/src/test/java/exemplo/restassured/reqres/testes/ValidarLista.java
@@ -0,0 +1,22 @@
+package exemplo.restassured.reqres.testes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class ValidarLista {
+	
+	Response response;
+	
+	@Test
+	public void validarLista() {
+		
+		response = RestAssured.get("http://reqres.in"); 
+		assertEquals(200, response.getStatusCode());
+		
+	}
+
+}
